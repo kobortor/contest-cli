5ce61015f258d60115768de371d7a9943ba6fef0
kobortor <kobortor@gmail.com>
2020-06-01 00:48:08 -0400

    bad install script

--- scripts/install.sh
+++ scripts/install.sh
@@ -3,8 +3,10 @@
 sudo cp dmoj /usr/local/bin/
 mkdir -p ~/.contest-cli/dmoj
 
-for f in defaults/{languages,patterns,settings}.json; do
-    if [ -f "$f" ]; then
+for f in {languages,patterns,settings}.json; do
+    if [ -f "~/.contest-cli/dmoj/$f" ]; then
         echo "File $f found, not overwriting... To overwrite, use 'make hard-install'"
+    else
+        cp "defaults/$f" "~/.contest-cli/dmoj/$f"
     fi
 done
