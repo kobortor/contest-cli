7a62ecba4390bdf0ff325a4b70761ca792aefdce
kobortor <kobortor@gmail.com>
2020-06-01 18:34:09 -0400

    Some operating systems apparently don't expand ~ properly

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
 sudo cp dmoj /usr/local/bin/
-mkdir -p ~/.contest-cli/dmoj
+mkdir -p "$HOME/.contest-cli/dmoj"
 
 for f in {languages,patterns,settings}.json; do
-    if [ -f "~/.contest-cli/dmoj/$f" ]; then
+    if [ -f "$HOME/.contest-cli/dmoj/$f" ]; then
         echo "File $f found, not overwriting... To overwrite, use 'make hard-install'"
     else
-        cp "defaults/$f" "~/.contest-cli/dmoj/$f"
+        cp "defaults/$f" "$HOME/.contest-cli/dmoj/$f"
     fi
 done
