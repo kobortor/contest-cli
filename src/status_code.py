84dc3480e398fbd0e20a4d42a3ba49ce1a17a2b4
kobortor <kobortor@gmail.com>
2020-06-15 20:36:05 -0400

    Added status code

---
+++ src/status_code.py
@@ -0,0 +1,25 @@
+from colorama import Fore, Back, Style
+
+codes = [
+    "AC",
+    "WA",
+    "IR",
+    "IE",
+    "AB",
+    "RTE",
+    "MLE",
+    "OLE",
+    "TLE",
+    "CE"
+]
+
+COLORED_AC = Fore.GREEN + "AC" + Fore.RESET
+COLORED_WA = Fore.RED + "WA" + Fore.RESET
+COLORED_IR = Fore.YELLOW + "IR" + Fore.RESET
+COLORED_IE = Fore.YELLOW + "IE" + Fore.RESET
+COLORED_AB = Fore.YELLOW + "AB" + Fore.RESET
+COLORED_RTE = Fore.YELLOW + "RTE" + Fore.RESET
+COLORED_MLE = Fore.YELLOW + "MLE" + Fore.RESET
+COLORED_OLE = Fore.YELLOW + "OLE" + Fore.RESET
+COLORED_TLE = Fore.LIGHTBLACK_EX + "TLE" + Fore.RESET
+COLORED_CE = Fore.LIGHTBLACK_EX + "CE" + Fore.RESET
